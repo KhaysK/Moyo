8f4967a2c91b9f53280fcf784cd813d79b464b00
Khabib <k.khaysadykov@innopolis.university>
2023-05-20 16:08:20 +0300

    Footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Header from "./components/Header";
 import "./styles/App.css"
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <NavBar/>
-      <Banner/>
+      <Header />
+      <div className="content">
+        <NavBar />
+        <Banner />
+      </div>
+      <Footer />
     </div>
   );
 }
