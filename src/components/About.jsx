44599a5b9bd5b9977ec9c19bb32f3416d6e30eef
Khabib <k.khaysadykov@innopolis.university>
2023-05-21 03:24:33 +0300

    Full About component implementation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,19 @@ import icon1 from '../materials/icon1.svg';
 import icon2 from '../materials/icon2.svg';
 import about from '../materials/aboutUs.svg';
 import clasess from './styles/About.module.css'
+import StatBlock from "../UI/about/StatBlock";
+import stat1 from "../materials/stat1.svg";
+import stat2 from "../materials/stat2.svg";
+import stat3 from "../materials/stat3.svg";
+import stat4 from "../materials/stat4.svg";
+import phone from "../materials/Phone.svg";
+import MyButton from "../UI/button/MyButton";
 
 function About() {
     return (
-        <div>
+        <div className={clasess.container}>
             <p style={{ fontSize: "36px", marginBottom:'42px' }}>О нас</p>
-            <div style={{ display: 'flex' }}>
+            <div style={{ display: 'flex', justifyContent:"center"}}>
                 <div className={clasess.about}>
                     <img src={logo} alt="Moyo logo" width={200} />
                     <p className={clasess.desription}>Наш благотворительный фонд основан с целью помощи людям, которые оказались в трудной жизненной ситуации. Мы стремимся помочь каждому, кто нуждается в нашей поддержке, независимо от возраста, пола, национальности или вероисповедания.</p>
@@ -20,6 +27,20 @@ function About() {
                     </div>
                 </div>
                 <img src={about} alt="Moyo logo" />
+            </div>
+            <div style={{display:"flex", gap:"24px"}}>
+                <StatBlock img={stat1} title={'Людей поддержали наш фонд'} data={'22 417'}/>
+                <StatBlock img={stat4} title={'Спасённых жизней'} data={'215'}/>
+                <StatBlock img={stat2} title={'Волонтера'} data={'332'}/>
+                <StatBlock img={stat3} title={'Миллиона рублей пожертвования'} data={'432'}/>
+            </div>
+            <div style={{display:"flex", gap:"100px", marginTop:"112px"}}>
+                <img src={phone} alt="phone image" />
+                <div style={{width:"907px", display:"flex", gap:"20px", flexDirection:"column"}}>
+                    <p style={{fontSize:"64px", fontWeight:"700"}}>Делайте добро всегда и везде с нашим мобильным приложением</p>
+                    <p style={{width:"678px", fontSize:"24px", color:"#888888", marginBottom:"20px"}}>Наше мобильное приложение для благотворительности - это удобный и простой способ сделать доброе дело в любое время и в любом месте. С его помощью вы можете выбрать проект или организацию, которым хотите помочь, и сделать пожертвование всего в несколько кликов.</p>
+                    <MyButton>Скачать</MyButton>
+                </div>
             </div>
         </div>
     );
